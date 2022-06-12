4407682398a923d2dbf488c4735e73096e9fac62
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-12 21:48:23 +0100

    Fixed a bug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,17 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, SimpleGrid } from "@chakra-ui/react";
 
 const Main = ({ movies }) => {
   return (
-    <Flex>
-      {movies.Search.map((item, index) => {
-        return (
-          <Box key={index}>
-            <Image src={item.Poster}></Image>
-          </Box>
-        );
-      })}
+    <Flex justify="center" p="5">
+      <SimpleGrid columns={[2, 3, 4]} maxW="1200px" w="100%" spacing="5">
+        {movies.Search?.map((item, index) => {
+          return (
+            <Box key={index}>
+              <Image src={item.Poster}></Image>
+            </Box>
+          );
+        })}
+      </SimpleGrid>
     </Flex>
   );
 };

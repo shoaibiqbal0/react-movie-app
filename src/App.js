af77c526bff62ccb7792f3e6461634009bbdcf7b
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-12 21:20:04 +0100

    Added state for search term

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { useState } from "react";
+import Main from "./components/Main";
+import Search from "./components/Search";
 
 function App() {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+
+  const apiKey = "4b056b87";
+  const url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
+
+  return (
+    <div>
+      <Search search={search} setSearch={setSearch} />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,21 @@
-const Search = () => {
-  return <div>Search</div>;
+import { Flex, Input } from "@chakra-ui/react";
+
+const Search = ({ search, setSearch }) => {
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <Flex justify="center" p="5">
+      <Input
+        onChange={handleChange}
+        maxW="800px"
+        borderRadius="full"
+        variant="filled"
+        value={search}
+      />
+    </Flex>
+  );
 };
 
 export default Search;

d67f0e6d6982a4994cabfcab2cbd7e54809a7ad5
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-13 19:32:17 +0100

    Added dark mode icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const Navbar = () => {
-  const [darkMode, setDardMode] = useState();
+  const [darkMode, setDarkMode] = useState(true);
+
+  const handleChange = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <VStack>
       <VStack p="5" w="100%" align="flex-end">
-        <IconButton icon={<FaSun />} isRound="true" />
+        <IconButton
+          icon={darkMode ? <FaSun /> : <FaMoon />}
+          isRound="true"
+          onClick={handleChange}
+        />
       </VStack>
       <Text fontSize="7xl" fontWeight="bold">
         Movie Search
